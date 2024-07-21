b46c2ca9c38ae51de7b1b0ba4d4a2fa620a8f24d
JAYASIRI S.P.A.S. <ict21860@fot.sjp.ac.lk>
2024-07-22 01:57:33 +0530

    Project File Setup

---
+++ app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+from streamlit_extras.add_vertical_space import add_vertical_space
+
+with st.sidebar:
+    st.title('🤗💬 LLM Chat App')
+    st.markdown('''
+    ## About
+    This app is an LLM-powered chatbot built using:
+    - [Streamlit](https://streamlit.io/)
+    - [LangChain](https://python.langchain.com/)
+    - [OpenAI](https://platform.openai.com/docs/models) LLM model
+ 
+    ''')
+    add_vertical_space(5)
+    st.write('Made with ❤️ by [Developer](https://portfolio-avishka-shehan-5dwot0xnv-avishka-shehans-projects.vercel.app/)')
