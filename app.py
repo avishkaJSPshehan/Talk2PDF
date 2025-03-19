ea9f48f5f96daa227f154e1fcbcb097c8cd9d93b
JAYASIRI S.P.A.S. <ict21860@fot.sjp.ac.lk>
2025-03-19 14:14:16 +0530

    Embedding Text

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 import streamlit as st
+from PyPDF2 import PdfReader
 from streamlit_extras.add_vertical_space import add_vertical_space
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
 
 #sidebar contents
 with st.sidebar:
@@ -13,5 +17,39 @@ with st.sidebar:
                 
     ''')
     add_vertical_space(3)
-    st.write('Made with ❤️ by Avishka Shehan')
+    st.write('Made with ❤️ by [Avishka Shehan](https://portfolio-avishka-shehan.vercel.app/)')
 
+
+def main():
+    st.write("Chat with PDF 💭")
+
+    # upload a PDF file
+    pdf = st.file_uploader("Upload your PDF", type='pdf')
+
+    #read the pdf content
+    if pdf is not None:
+        pdf_reader = PdfReader(pdf)
+        
+        text = ""
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+
+        #use text splitter to split the whole text
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=1000,
+            chunk_overlap=200,
+            length_function=len
+        )
+
+        chunks = text_splitter.split_text(text=text)
+
+        #embeddings
+        embeddings = OpenAIEmbeddings()
+
+        VectorStore = FAISS.from_texts(chunks,embedding=embeddings)
+        
+
+
+
+if __name__ == '__main__':
+    main()
